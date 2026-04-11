68e816aea0df7a200349f7b2be1d75b2014592ed
PRORAINDORP <santiagoclarencejames24@gmail.com>
2026-04-11 15:37:01 +0800

    add settings for Python environment and update Car class with color function

--- Car.py
+++ Car.py
@@ -4,6 +4,13 @@ class car():
         self.name = name
         self.color = color
         self.boost = boost 
-    def accelerate(self):
-        self.speed += self.boost
+    def __str__(self):
+        return f"{self.name} is a {self.color} car with a speed of {self.speed} and a boost of {self.boost}"
+
+def colors():
+    red = (255, 0, 0)
+    blue = (0, 0, 255)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    return red, blue, black, white
 
--- PixelVelocity.py
+++ PixelVelocity.py
@@ -1,8 +1,12 @@
 import pygame
 import Car
+import random
 
 class pixelvelocity():
     def __init__(self, car, track):
         self.car = car
         self.track = track
-    def calculate_velocity(self):
+
+p1 = Car.car(2, "Player", "Red", 1)
+Ai = Car.car(2, "Ai", Car.colors()[1], 1)
+print(Ai)

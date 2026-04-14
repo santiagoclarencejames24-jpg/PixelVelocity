862ab050eaa79f05f89deba06b0454f5914c7f39
PRORAINDORP <santiagoclarencejames24@gmail.com>
2026-04-14 23:24:07 +0800

    add game initialization and main loop to PixelVelocity.py, include background image

--- PixelVelocity.py
+++ PixelVelocity.py
@@ -11,5 +11,32 @@ class pixelvelocity():
 
 p1 = Car.car(2, "Player", Car.car.colors()[0], 1)
 Ai = Car.car(2, "Ai", Car.car.colors()[1], 1)
-print(p1)
-print(Ai)
+
+#pygame design
+pygame.init()
+
+
+width = 1925
+height = 1025
+screen = pygame.display.set_mode((width, height))
+
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Pixel Velocity")
+
+image = pygame.image.load("images/background.jpg")
+
+is_active = True
+
+while is_active:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_active = False
+
+    screen.blit(image, (0, 0))
+    pygame.draw.rect(screen, "grey", (0, 500, 1925, 300))
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
